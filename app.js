547c81b4aa6e470992e44158de5155cad2a44547
Cate Dunn <60521577+CateDunn@users.noreply.github.com>
2020-09-10 13:24:01 -0400

    filter task function added

--- app.js
+++ app.js
@@ -16,6 +16,8 @@ function loadEventListeners(){
   taskList.addEventListener('click', removeTask);
   //clear task event
   clearBtn.addEventListener('click', clearTasks);
+  //filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 };
 
 //Add Task Function
@@ -60,3 +62,18 @@ function clearTasks(e){
     taskList.removeChild(taskList.firstChild);
   };
 };
+
+//Filter Tasks Function
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+  
+  document.querySelectorAll('.collection-item').forEach
+  (function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block'
+    } else {
+      task.style.display = 'none'
+    }
+  });
+}
